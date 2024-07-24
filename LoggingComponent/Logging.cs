aa1804630f13ff1822b268be5e825326f92628c5
Steven Gong <gonghaima@hotmail.com>
2024-07-24 19:16:07 +1000

    add conditional attribute

--- LoggingComponent/Logging.cs
+++ LoggingComponent/Logging.cs
@@ -1,7 +1,9 @@
-﻿namespace LoggingComponent;
+﻿using System.Diagnostics;
+namespace LoggingComponent;
 
 public class Class1
 {
+    [Conditional("LOG_INFO")]
     public static void Log(string message)
     {
         Console.WriteLine(message);
